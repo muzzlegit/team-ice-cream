462fba6ef1d95db4e150c6c4ee7763e80753000c
Roman Vozniuk <muzzlestore@gmail.com>
2021-07-20 11:54:31 +0300

    прикрутил кнопку модалки  и id на секции

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,13 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtnSec: document.querySelector('[data-modal-menu-open]'),    
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-      open: document.querySelector('[data-open]')
+    open: document.querySelector('[data-open]')
   };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
+ refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtnSec.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() { 
